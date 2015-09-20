b5d03731d6757eeeb4631e700bb308989c1fa9b3
guoqianyou <guoqianyou@163.com>
2015-09-20 14:45:55 +0800

    家里提交

---
+++ src/main/java/utils/RequestNews.java
@@ -0,0 +1,25 @@
+package utils;
+
+import java.util.Map;
+
+/**
+ * @author GQY
+ *         Created by GQY on 2015/9/16.
+ *         处理request请求来的数据
+ */
+public class RequestNews {
+    /**
+     * 根据名字在paramters中获得数据
+     * @param name 在paramters中的名字
+     * @param paramters  在这个paramters中获得
+     * @return 返回获得的数据
+     */
+    public static Object getParamters(final String name, final Map<String, Object> paramters) {
+        if (null == name || null == paramters.get(name)) {
+            return null;
+        } else {
+            Object o = paramters.get(name);
+            return o;
+        }
+    }
+}
--- src/main/java/web/ajax/AjaxRequest.java
+++ src/main/java/web/ajax/AjaxRequest.java
@@ -1,33 +1,29 @@
 package web.ajax;
 
-import web.Head;
+import utils.JsonNews;
+import utils.RequestNews;
+import web.RequestBean;
 
-import java.io.Serializable;
-import java.util.Objects;
+import java.util.Map;
 
 /**
  * @author GQY
- * Created by GQY on 2015/9/14.
+ *         Created by GQY on 2015/9/14.
  */
-public class AjaxRequest implements Serializable {
+public class AjaxRequest extends RequestBean {
 
     private static final long serialVersionUID = -938759752665532982L;
-    private Head head;
-    private Objects body;
+    private Map<String, Object> parameters;
 
-    public Head getHead() {
-        return head;
+    public AjaxRequest(Map<String, Object> parameters) {
+        this.parameters = parameters;
     }
 
-    public void setHead(Head head) {
-        this.head = head;
-    }
-
-    public Objects getBody() {
-        return body;
-    }
-
-    public void setBody(Objects body) {
-        this.body = body;
+    protected <T> T reload(Class<T> clas, String name) {
+        Object object = RequestNews.getParamters(name, this.parameters);
+        if (null != object) {
+            return (T) JsonNews.jsonToObject(object.toString(), clas);
+        }
+        return null;
     }
 }
