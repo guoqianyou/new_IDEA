34984e2b11b433718509c2f9d5f8ff56312e1637
guoqianyou@163.com <guoqianyou@163.com>
2015-09-24 09:38:30 +0800

    公司

--- src/main/java/web/ajax/AjaxAction.java
+++ src/main/java/web/ajax/AjaxAction.java
@@ -1,12 +1,42 @@
 package web.ajax;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
+import web.RequestBean;
+import web.ResponseBean;
+import web.service.ServiceExcute;
 
 /**
  * @author GQY
- * Created by GQY on 2015/9/14.
+ *         Created by GQY on 2015/9/14.
  */
-public class AjaxAction extends ActionSupport{
+public class AjaxAction extends ActionSupport {
     private static final long serialVersionUID = -4361321323693253764L;
 
+    @Override
+    public String execute() throws Exception {
+        AjaxRequest request = new AjaxRequest(ActionContext.getContext().getParameters());
+        AjaxResponse response = new AjaxResponse();
+        responseBean = ServiceExcute.execute(request, response);
+        return SUCCESS;
+    }
+
+    private RequestBean requestBean;
+    private ResponseBean responseBean;
+
+    public RequestBean getRequestBean() {
+        return requestBean;
+    }
+
+    public void setRequestBean(RequestBean requestBean) {
+        this.requestBean = requestBean;
+    }
+
+    public ResponseBean getResponseBean() {
+        return responseBean;
+    }
+
+    public void setResponseBean(ResponseBean responseBean) {
+        this.responseBean = responseBean;
+    }
 }
