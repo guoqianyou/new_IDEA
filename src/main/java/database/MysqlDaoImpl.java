b86855c8448db327909dc0cc003ea60fcc408455
guoqianyou <guoqianyou@163.com>
2015-09-29 00:32:10 +0800

    家里提交

--- src/main/java/database/MysqlDaoImpl.java
+++ src/main/java/database/MysqlDaoImpl.java
@@ -3,10 +3,10 @@ package database;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.Resource;
 import java.io.Serializable;
 import java.util.List;
 
@@ -19,7 +19,7 @@ public class MysqlDaoImpl<T> implements MysqlDao<T> {
         return sessionFactory;
     }
 
-    @Autowired
+    @Resource(name = "sessionFactory")
     public void setSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
--- src/test/java/Ttst.java
+++ src/test/java/Ttst.java
@@ -1,10 +1,25 @@
+import database.MysqlDao;
+import database.model.UserInfomation;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by guoqi on 2015/9/12.
  */
 public class Ttst {
-    public static void main(String[] args) {
-        String string = "112321daidfn-adf";
-        String[] a =string.split("\\|");
-        System.out.println("args = [" + a.toString()+ "]");
+//    public static void main(String[] args) {
+//        String string = "112321daidfn-adf";
+//        String[] a =string.split("\\|");
+//        System.out.println("args = [" + a.toString()+ "]");
+//    }
+    @Test
+    public void ttest(){
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MysqlDao dao= (MysqlDao) ctx.getBean("mysqlDao");
+        UserInfomation user = new UserInfomation();
+        user.setUserName("guoqianyou");
+        user.setPassword("1234");
+        dao.save(user);
     }
 }
